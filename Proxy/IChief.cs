eb7e3df7ac841f38420e965becd8a065a5fb041f
Kobsen <kobsen2202@gmail.com>
2022-08-05 16:01:05 +0300

    Add: Proxy

---
+++ Proxy/IChief.cs
@@ -0,0 +1,7 @@
+﻿namespace Proxy;
+
+public interface IChief
+{
+    IEnumerable<Order> GetOrders();
+    IDictionary<int, string> GetStatuses();
+}
---
+++ Proxy/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Proxy;
+
+public struct Order
+{
+    public Order(Guid id, string? name, int statusId)
+    {
+        Id = id;
+        Name = name;
+        StatusId = statusId;
+    }
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Name { get; set; }
+    public int StatusId { get; set; }
+}
